6e620b6ca569ef898c02979d82b381f98562f0b2
daw2-viri23 <virimurillojoseluis@fpllefia.com>
2022-12-19 17:38:18 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-const path = require('path')
+const { resolve } = require('path')
 
-export default {
-  root: path.resolve(__dirname, 'src'),
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
-  server: {
-    port: 8080,
-    hot: true
-  }
+module.exports = {
+    root: resolve(__dirname, './src'),
+    build: {
+        rollupOptions: {
+            /*
+                Opciones de configuración de Rollup externas, serán mergeadas con la configuracion
+                interna de Rollup de Vite.
+            */
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                about: resolve(__dirname, 'src/index.html'),
+            },
+            output: {
+                dir: resolve(__dirname, 'build'), //Donde se va a crear el build de nuestra aplicacion
+                format: 'es', //Formato de ES modules
+            },
+        },
+        outDir: resolve(__dirname, 'build'),
+        //minify: false, //( Si no se quiere minificar el build) https://vitejs.dev/config/#build-minify (aplica solo a los JS no CSS)
+    },
 }
